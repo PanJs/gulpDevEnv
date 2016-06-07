c90cf24aff1f51d4c128b52484dcaa43faaf39b0
zhaiduo <zhaiduo@gmail.com>
2016-06-07 13:39:02 +0800

    new: mocha tests

--- test/gulp.js
+++ test/gulp.js
@@ -1,4 +1,5 @@
 'use strict';
+//http://mochajs.org/
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
@@ -29,6 +30,8 @@ var merge = require('merge-stream');
 var fs = require('fs');
 var gulpif = require('gulp-if');
 
+var expect = require("chai").expect;
+
 //http://www.ituring.com.cn/article/54547
 var Q = require('q');
 var karmaServer = require('karma').Server;
@@ -36,6 +39,14 @@ var karmaServer = require('karma').Server;
 var _red = function(title, info) {
     console.log(chalk.white.bgRed.bold(' ' + title + ' '), info);
 };
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+var rmdir = require('rimraf');
 
 //////////配置目录名称
 
@@ -48,147 +59,13 @@ var targetDirIsInside = true;
 var prefixOfTargetDir = (targetDirIsInside) ? '' : '../';
 //////////配置目录名称
 
-//sourceDirName和targetDirName只能包含0-9a-z_
-sourceDirName = sourceDirName.replace(/[\r\n]/ig, "");
-targetDirName = targetDirName.replace(/[\r\n]/ig, "");
-if (sourceDirName === "") {
-    console.log(chalk.white.bgRed.bold('[error]') + ' 缺少 sourceDirName');
-    process.exit()
-}
-if (targetDirName === "") {
-    console.log(chalk.white.bgRed.bold('[error]') + ' 缺少 targetDirName');
-    process.exit()
-}
-
-//deploy前请gulp karma进行测试
-gulp.task('karma', function(done) {
-    new karmaServer({
-        configFile: __dirname + '/karma.test.conf.js',
-        singleRun: true
-    }, done).start();
-});
-
-//less生成压缩版css
-gulp.task('lessmin', function() {
-    return gulp.src('./css/*.less')
-        .pipe(less({
-            paths: [path.join(__dirname, 'css')],
-            plugins: [cleancss]
-        }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream({
-            match: "**/*.css"
-        }));
-});
-
-//生成css，不压缩
-gulp.task('less', function() {
-    return gulp.src('./css/**/*.less')
-        .pipe(less({
-            paths: [path.join(__dirname, 'css')]
-        }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream({
-            match: "**/*.css"
-        }));
-});
-
-gulp.task('cleanCssFiles', function() {
-    return gulp.src(['./css/**/*.css'], {
-            read: false
-        })
-        .pipe(clean());
-});
-
-var chgdFiles = {};
-var chgdCss = {};
-var chgdLess = {};
-
-//watch:Django时是否压缩css/js
-var isCompress = true;
-
-//Django文件监控(压缩)
-//gulp watch:Django --src=yes/no
-gulp.task('watch:Django', function() {
-
-    if (typeof argv.src == 'string' && argv.src.match(/^(yes|no)$/i)) isCompress = (RegExp.$1.match(/^yes$/i)) ? false : true;
-    if (isCompress) {
-        console.log(chalk.white.bgGreen.bold('［压缩模式］'));
-    } else {
-        console.log(chalk.white.bgMagenta.bold('［非压缩模式］'));
-    }
-    console.log(chalk.white.bgBlue.bold('［请注意］：') + chalk.black.bgWhite.bold(' push／pull代码时请暂停watch:Django文件监控［Ctrl + c］'));
-    browserSync.init({
-        server: {
-            baseDir: "./" + sourceDirName + "/",
-            directory: true
-        },
-        //不弹窗
-        open: false,
-        //notify: false
-        //startPath: "/index.html"
-        //scrollProportionally: false // Sync viewports to TOP position
-        //scrollThrottle: 100 // only send scroll events every 100 milliseconds
-        //https://www.browsersync.io/docs/options/
-        port: 4032
-    });
-    //Default: ['add', 'change', 'unlink']
-    gulp.watch("./" + sourceDirName + "/**/*.html").on("change", function(chg) {
-        console.log('watch', chg);
-        gulp.start('dev', function(done) {
-            browserSync.reload();
-        });
-    });
-    //如果文件有修改，编译less到目录
-    gulp.watch("./" + sourceDirName + "/css/**/*.less").on("change", function(chg) {
-        console.log('watch', chg);
-        gulp.start('dev', function(done) {
-            browserSync.reload();
-        });
-    });
-    //如果文件有修改，编译less到public目录(同一个目录less=>css)
-    gulp.watch("./" + sourceDirName + "/**/*.less").on("change", function(chg) {
-        console.log('watch', chg);
-        if (chg.type == 'changed') {
-            //有文件被修改
-            chgdLess = chg;
-            gulp.start('deploy_local_less', function(done) {
-                browserSync.reload();
-            });
-        }
-    });
-    //如果文件有修改，发布页面css代码
-    gulp.watch("./" + sourceDirName + "/**/*.css").on("change", function(chg) {
-        console.log('watch', chg);
-        if (chg.type == 'changed') {
-            //有文件被修改
-            chgdCss = chg;
-            gulp.start('deploy_local_css', function(done) {
-                browserSync.reload();
-            });
-        }
-    });
-    //如果文件有修改，发布页面js代码
-    gulp.watch("./" + sourceDirName + "/**/*.js").on("change", function(chg) {
-        //{ type: 'changed', path: '' }
-        console.log('watch', chg);
-        if (chg.type == 'changed') {
-            //有文件被修改
-            chgdFiles = chg;
-            gulp.start('deploy_local_js', function(done) {
-                browserSync.reload();
-            });
-        }
-    });
-
-});
-
 var syncLess = function(chgdFiles, trgDirName) {
     if (chgdFiles.path != undefined && chgdFiles.path != null && chgdFiles.path.match(/\.(less)$/i)) {
         if (chgdFiles.path.match(/^(.*)\/([^\/]+)$/i)) {
             var basePath = RegExp.$2;
             var destDir = RegExp.$1 + '/';
-            var reg = new RegExp("^(.+)\/" + sourceDirName + "\/(.+)\/([^\/]+)$", "i");
+            var reg = new RegExp("^(.+)\/(" + sourceDirName + "\/)?(.+)\/([^\/]+)$", "i");
+            //console.log('syncLess', chgdFiles.path, reg);
             if (chgdFiles.path.match(reg)) {
                 //console.log('destDir', basePath, destDir);
                 var go = function() {
@@ -208,26 +85,19 @@ var syncLess = function(chgdFiles, trgDirName) {
                     go()
                 ]);
                 allPromise.then(function(data) {
-                    console.log('finished', data, '../' + trgDirName + '/' + data[0].destDir + data[0].basePath);
-                    var deferred2 = Q.defer();
-                    return deferred2.promise;
+                    console.log('finished', data, prefixOfTargetDir + trgDirName + '/' + data[0].destDir + data[0].basePath);
+                    //var deferred2 = Q.defer();
+                    //return deferred2.promise;
                 }, function(data) {
                     console.log('error', data);
-                    return deferred.promise;
+                    //return deferred.promise;
                 });
 
             }
+            return [prefixOfTargetDir + trgDirName + '/' + destDir + basePath];
         }
     }
 };
-//如果文件有修改，同步最新less到目录 ./开发目录/ to ./发布目录/
-gulp.task('deploy_local_less', ['dev'], function() {
-    console.log('deploy_local_less');
-    var all;
-    var chgdFiles = chgdLess;
-    syncLess(chgdFiles, targetDirName);
-});
-
 
 var syncCss = function(chgdFiles, isCompress, trgDirName) {
     if (chgdFiles.path != undefined && chgdFiles.path != null && chgdFiles.path.match(/\.(css)$/i) && !chgdFiles.path.match(/\.min\.(css)$/i)) {
@@ -281,12 +151,6 @@ var syncCss = function(chgdFiles, isCompress, trgDirName) {
         }
     }
 };
-//如果文件有修改，同步最新css到目录 ./public/ to ../public/
-gulp.task('deploy_local_css', function() {
-    var all;
-    var chgdFiles = chgdCss;
-    syncCss(chgdFiles, isCompress, targetDirName);
-});
 
 var syncJs = function(chgdFiles, isCompress, trgDirName) {
     var all;
@@ -297,7 +161,7 @@ var syncJs = function(chgdFiles, isCompress, trgDirName) {
             var reg = new RegExp("^(.+)\/(" + sourceDirName + "\/)?(.+)\/([^\/]+)$", "i");
             if (chgdFiles.path.match(reg)) {
                 destDir = RegExp.$3 + '/';
-                console.log('destDir', prefixOfTargetDir + trgDirName + '/' + destDir + basePath);
+                console.log('syncJs', prefixOfTargetDir + trgDirName + '/' + destDir + basePath);
                 all = gulp.src([
                     chgdFiles.path
                 ])
@@ -317,42 +181,79 @@ var syncJs = function(chgdFiles, isCompress, trgDirName) {
             }
         }
     }
-    return all;
+    //return all
+    return [prefixOfTargetDir + trgDirName + '/' + destDir + basePath];
 };
-//发布页面js代码 ./public/ to ../public/
-gulp.task('deploy_local_js', function() {
-    var all = syncJs(chgdFiles, isCompress, targetDirName);
-    return merge(
-        all
-    );
-});
 
-gulp.task('reload', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            directory: true
-        }
+//////start test
+
+/*describe("gulpfile test: syncLess", function() {
+    var chgdFiles = {};
+    chgdFiles.path = __dirname + '/../dev/test.less';
+    var trgDirName = targetDirName;
+    var targetFile = __dirname + '/../' + trgDirName + '/dev/test.less';
+    var isTargetExist = null;
+    var isTargetExist2 = null;
+    if (fs.existsSync(targetFile)) {
+        isTargetExist = true;
+    } else {
+        isTargetExist = false;
+    }
+    if (isTargetExist) {
+        rmdir(__dirname + '/../' + trgDirName + '/dev', function(error) {
+            //console.log('failed to clean test files', error);
+        });
+    }
+    var data = syncLess(chgdFiles, trgDirName);
+    it("syncLess", function() {
+        //setTimeout(function() {
+            console.log('data', data);
+            expect(sourceDirName).equal('dev');
+            expect(chgdFiles.path).match(/test\.less/i);
+            expect(trgDirName).equal('dist');
+            expect(prefixOfTargetDir).equal('');
+            expect(isTargetExist).equal(false);
+
+            //expect(isTargetExist2).equal(true);
+            expect(data.length).equal(1);
+            expect(data[0]).match(/test\.less/i);
+        //}, 1000);
     });
-    browserSync.reload
-});
 
-gulp.task('gallery', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./gallery/",
-            directory: true
-        }
+});*/
+
+describe("gulpfile test: syncJs", function() {
+    var chgdFiles = {};
+    chgdFiles.path = __dirname + '/../dev/test.js';
+    var isCompress = true;
+    var trgDirName = targetDirName;
+    var targetFile = __dirname + '/../' + trgDirName + '/dev/test.js';
+    var isTargetExist = null;
+    var isTargetExist2 = null;
+    if (fs.existsSync(targetFile)) {
+        isTargetExist = true;
+    } else {
+        isTargetExist = false;
+    }
+    if (isTargetExist) {
+        rmdir(__dirname + '/../' + trgDirName + '/dev', function(error) {
+            //console.log('failed to clean test files', error);
+        });
+    }
+    var data = syncJs(chgdFiles, isCompress, trgDirName);
+    it("syncJs", function() {
+        //setTimeout(function() {
+            expect(sourceDirName).equal('dev');
+            expect(chgdFiles.path).match(/test\.js/i);
+            expect(trgDirName).equal('dist');
+            expect(prefixOfTargetDir).equal('');
+            expect(isTargetExist).equal(false);
+
+            expect(isTargetExist2).equal(true);
+            expect(data.length).equal(2);
+            expect(data[1]).match(/test\.js/i);
+        //}, 1000);
     });
-    //browserSync.reload
+
 });
 
-////////////////////////////
-//重新发布测试环境
-gulp.task('dev', [
-    'cleanCssFiles', 'less'
-]);
-//启动默认开发环境： gulp
-gulp.task('default', [
-
-]);
